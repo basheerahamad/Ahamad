dbe583b7cc9a930d717dce3ea22805bdeb43e65e
hi <hi@DESKTOP-P60UAQ7>
2019-06-25 12:17:46 +0530

    added class Arguments.java

---
+++ Ahamad/Arguments.java
@@ -0,0 +1,19 @@
+
+public class Arguments {
+	
+	
+	    public static void main (String[] args) 
+	    {
+	        if (args.length ==0)
+	            {
+	            System.out.println("No Values");
+	    }
+	        else{
+	            for(String i : args)     
+	                System.out.print(i+" , ");
+	        
+	        }
+	        }
+	        }
+
+
